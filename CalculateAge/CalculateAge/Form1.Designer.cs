80b6f1b8783ce9f9d73f147519be860b65296ef8
Robert Wisniewski <48819648+hesa656@users.noreply.github.com>
2020-09-24 23:26:39 +0200

    Add files via upload

---
+++ CalculateAge/CalculateAge/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace CalculateAge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calendar = new System.Windows.Forms.MonthCalendar();
+            this.calculate_bttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(24, 40);
+            this.calendar.Name = "calendar";
+            this.calendar.TabIndex = 0;
+            this.calendar.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // calculate_bttn
+            // 
+            this.calculate_bttn.Location = new System.Drawing.Point(24, 208);
+            this.calculate_bttn.Name = "calculate_bttn";
+            this.calculate_bttn.Size = new System.Drawing.Size(269, 23);
+            this.calculate_bttn.TabIndex = 1;
+            this.calculate_bttn.Text = "Calculate!";
+            this.calculate_bttn.UseVisualStyleBackColor = true;
+            this.calculate_bttn.Click += new System.EventHandler(this.calculate_bttn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 270);
+            this.Controls.Add(this.calculate_bttn);
+            this.Controls.Add(this.calendar);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CalculateAge";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar calendar;
+        private System.Windows.Forms.Button calculate_bttn;
+    }
+}
+
---
+++ CalculateAge/CalculateAge/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculateAge
+{
+    public partial class Form1 : Form
+    {
+        int birthYear;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            birthYear = e.Start.Year; //Get selected Year in calendar
+        }
+
+        private void calculate_bttn_Click(object sender, EventArgs e)
+        {
+            var age = DateTime.Now.Year - birthYear; //Calculate Age (Actual year minus Your birthday year)
+
+            Console.WriteLine("DateTime.Now.Year: " + DateTime.Now.Year + "\nbirthYear: " + birthYear + "\nResult: " + age);
+
+            MessageBox.Show("You are " + age + " years old!", "That's it!", MessageBoxButtons.OK, MessageBoxIcon.Information); //Show MessageBox with result
+        }
+    }
+}
